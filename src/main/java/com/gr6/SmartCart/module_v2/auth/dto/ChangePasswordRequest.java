9a3b1aa8d444392dcd43c1b8c5401bcf4eaa2a8e
devsangd3ypyy2 <sangd3ypyy2@gmail.com>
2026-05-27 06:07:58 +0700

    modify shop

---
+++ src/main/java/com/gr6/SmartCart/module_v2/auth/dto/ChangePasswordRequest.java
@@ -0,0 +1,4 @@
+package com.gr6.SmartCart.module_v2.auth.dto;
+
+public class ChangePasswordRequest {
+}
---
+++ src/main/java/com/gr6/SmartCart/modules/identity/dto/GoogleLoginRequest.java
@@ -0,0 +1,4 @@
+package com.gr6.SmartCart.modules.identity.dto;
+
+public class GoogleLoginRequest {
+}
